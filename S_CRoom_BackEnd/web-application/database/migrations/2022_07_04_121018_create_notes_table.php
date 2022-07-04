d4797f8254940be622e78297ed0e9b3092a32ed8
abody <abody99gd@gmail.com>
2022-07-04 15:08:09 +0200

    i made database_table

---
+++ S_CRoom_BackEnd/web-application/database/migrations/2022_07_04_121018_create_notes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id('note_id');
+            $table->foreignId('student_id')->constrained('students');
+            $table->foreignId('professor_id')->constrained('professor');
+            $table->timestamp();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+};
