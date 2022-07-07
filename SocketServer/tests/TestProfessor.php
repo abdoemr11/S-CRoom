35f62df66590513d4ee1876415f91c52adc37128
abody <abody99gd@gmail.com>
2022-07-07 04:13:30 +0200

    gad_10func_finish

--- SocketServer/tests/TestProfessor.php
+++ SocketServer/tests/TestProfessor.php
@@ -11,9 +11,9 @@ class TestProfessor extends \PHPUnit\Framework\TestCase
         //        $this->markTestSkipped("waiting to sign new student first");
 
         $arr = array(
-            'action' => 'connect',
-            'from' => 'student', //|prof|admin
-            'device_id' => '12345');
+            "action" => "connect",
+            "from" => "student", //|prof|admin
+            "device_id" => "12345");
 
         $socket = new Socket($this->address, $this->service_port);
         $socket->connect();
@@ -27,47 +27,75 @@ class TestProfessor extends \PHPUnit\Framework\TestCase
 
 
     }
-    public function testStartExamForProfessor() {
 
-        $arr = array([
-            "action" => "startexam",
-            "to"=> "student",
-            "from" => "proffessor",
-            'studentname'=>"Mohammed",
-            "device id" => "123",
-            "execute" => [[ 'question'=> 'hwat is your name?', 'correctanswer'=> 'Tantawy',
-                'incorrectanswers'=> ['Hossam', 'Gi', 'asdfadf']]]]);
-        echo "hi";
-        $socket = new Socket($this->address, $this->service_port);
-        $socket->connect();
-        $socket->send(Message_Handler::encode_msg($arr));
 
-//        $out = $socket->recv();
-    }
-    public function voteForProfessor() {
-
-        echo "hi";
-        $socket = new Socket($this->address, $this->service_port);
-        $socket->connect();
-        $socket->send(Message_Handler::encode_msg($arr));
-
-//        $out = $socket->recv();
-    }
-
-    public function testOpenCamForProfessor()
+    public function testStartExamForProfessor()
     {
-        $arr = array(
-            'action' => 'open_cam_for_prof',
-            'from' => 'proffessor', //|prof|admin
-            'to' => 'student',
-            'device_id' => '1',
-            'student_name' => 'hossam',
-            'execute' => ['']
+
+        $arr = array
+        ([
+                "action"      => "startexam"  ,
+                "to"          => "student"    ,
+                "from"        => "proffessor" ,
+                "studentname" => "Mohammed"   ,
+                "device id"   => "devicelD"   ,
+                "execute"     =>
+[
+ [ "question" =>  "hwat is your name?"       , "correctanswer"  => "Tantawy "     , "incorrectanswers"       =>    ["Hossam", "G "   ]],
+ [ "question" =>  "How old are you?"         , "correctanswer"  => "22"           , "incorrectanswers"       =>    ["20", "21", "19" ]],
+ [ "question" =>  "where are you from?"      , "correct answer" =>  "benha"       , "incorrectanswers"       =>    ["giza", "aswan"  ]],
+ [ "question" =>  "what is your job?"        , "correctanswer"  => "still student", "incorrectanswers"       =>    ["engineer"       ]],
+ [ "question" =>  "predicted graduated year?", "correctanswer"  => "2022"         , "incorrectanswers"       =>    ["2021", "21"     ]],
+ [ "question" =>  "your best friend?"        , "correctanswer"  => "Tantawy"      , "incorrectanswers"       =>    ["sayed", "saber" ]]
+]
+        ]);
+        echo "hi_start_exam";
+        $socket = new Socket($this->address, $this->service_port);
+        $socket->connect();
+        $socket->send(Message_Handler::encode_msg($arr));
+
+//        $out = $socket->recv();
+    }
+    public function voteForProfessor         ()
+    {
+        $arr = array
+        (
+        "action"      =>   "vote"           ,
+        "to"          =>   "student"        ,
+        "from"        =>   "proffessor"     ,
+        "student name"=>  "Mohammed"        ,
+        "device id"   =>   "device ID"      ,
+        "execute"     =>
+            [
+              "question"    =>  "what is your best color" ,
+              "first"       =>  "red"                     ,
+              "second"      =>  "blue"                    ,
+              "third"       =>  "black"                   ,
+              "fourth"      =>  "white"
+            ]
+    );
+        echo "hi";
+        $socket = new Socket($this->address, $this->service_port);
+        $socket->connect();
+        $socket->send(Message_Handler::encode_msg($arr));
+
+//        $out = $socket->recv();
+    }
+    public function testOpenCamForProfessor   ()
+    {
+        $arr = array
+        (
+            "action"      => "open cam for prof",
+            "to"          => "student"          ,
+            "from"        => "proffessor"       ,
+            "student name"=> "hosam"            ,
+            "device id"   => "device ID"        ,
+            "execute"     => "none"
         );
 
         $socket = new Socket($this->address, $this->service_port);
         $socket->connect();
-//        sleep(5);
+        //sleep(5);
         $socket->send(Message_Handler::encode_msg($arr));
         echo "Closing socket...";
         //return done and student id from student device
@@ -76,17 +104,24 @@ class TestProfessor extends \PHPUnit\Framework\TestCase
         //receive ok statement
 
     }
-    public function testOpenCamForAdmin() {
+    public function testOpenCamForAdmin       (){
 //                $this->markTestSkipped("waiting to sign new student first");
 
-        $arr = array(
-            'action' => 'open_cam_for_admin',
-            'from' => 'adminstrator', //|prof|admin
-            'to' => 'student',
-            'device_id' => '1',
-            'pic_nums' => 10,
-            'execute' => ['student_name' => 'abdo', 'n.o pictures' => 10]
-            );
+        $arr = array
+        ([
+                "action"       => "open cam for admin"         ,
+                "to"           => "student"                    ,
+                "from"         => "adminstrator"               ,
+                "student name" =>"hosam"                       ,
+                "device id"    =>"device ID"                   ,
+                "execute"      =>
+                    [
+                        "student name" => "khaled" ,
+                        "n.o pictures" => 20       ,
+                        "student ID"   => 50
+                    ]
+        ]);
+
 
         $socket = new Socket($this->address, $this->service_port);
         $socket->connect();
@@ -100,7 +135,21 @@ class TestProfessor extends \PHPUnit\Framework\TestCase
         //receive ok statement
 
     }
-    public function testTrainForProfessor() {
+    public function testTrainForProfessor     ()
+    {
+        $arr = array
+        ([
+                    "action"      => "train"       ,
+                    "to"          => "none"        ,
+                    "from"        => "adminstrator",
+                    "student name"=> "none"        ,
+                    "device id"   => "none"        ,
+                    "execute"     => "none"
+        ]);
+        echo "hi_train_message";
+        $socket = new Socket($this->address, $this->service_port);
+        $socket->connect();
+        $socket->send(Message_Handler::encode_msg($arr));
 
     }
 
--- SocketServer/tests/TestStudent.php
+++ SocketServer/tests/TestStudent.php
@@ -4,7 +4,8 @@ use MyApp\Utilities\Message_Handler;
 use MyApp\Utilities\Socket;
 class TestServer extends TestCase
 {
-    public function testConnect()
+
+    public function testConnect                     ()
     {
     //        $this->markTestSkipped("waiting to sign new student first");
 
@@ -25,30 +26,131 @@ class TestServer extends TestCase
     $this->assertEquals('you have been  in', Message_Handler::decode_msg($out)['data']);
 
     }
-    public function testResponseStartExam() {
+    public function testResponseStartExam           ()
+    {
 
+        if (studend id == "not_exist " )
+            $arr = array
+            ([
+                    "action" => " failed",
+                    " to " => "proffessor ",
+                    " from" => " student ",
+                    " student_name" => msg [" student_name "],
+                    "device_id" => "device_ID",
+                    " student_id" => "studend_id",
+                    " device_id " => "device_ID",
+                    " execute" =>
+                        [
+                            " student_id" => "studend_id",
+                            " device_id " => "device_ID",
+                            " student_name" => msg [" student_name "],
+                            " cause" => "not in data base "
+                        ]
+                ]);
 
-        echo "hi";
+            $arr = array
+            ([
+                    " action" => " done ",
+                    " to" => " proffessor ",
+                    " from" => " student ",
+                    "student_name" => msg [" student_name"],
+                    " device_id" => "device_ID",
+                    " execute" =>
+                        [
+                            "studentid" => "student_id",
+                            "device_id" => "device_ID",
+                            "student_name" => "results [0]",
+                            "correct " => "results [1]",
+                            "wrong " => "results [2]",
+                            " grade" => "results [3]",
+                            " array" => "results [4]"
+                        ]
+                ]);
+
         $socket = new Socket($this->address, $this->service_port);
         $socket->connect();
+        //sleep(5);
+        $socket->send(Message_Handler::encode_msg($arr));
+    }
+    public function testResponseVote                ()
+    {
+        $arr = array
+        ([
+                "action"       => "failed"              ,
+                "to"           => "proffessor"          ,
+                "from"         => "student"             ,
+                "student_name"  => msg["student_name"]   ,
+                "device_id"     => "device_ID"           ,
+                "execute"      =>
+                    [
+                        "student_id"       => "studendid"        ,
+                        "device_id"        => "device_ID"        ,
+                        "student_name"     => msg["student_name"] ,
+                        "cause"           => "not in data base"
+
+                    ]
+        ]);
+        
+
+
+        $socket = new Socket($this->address, $this->service_port);
+        $socket->connect();
+        //sleep(5);
         $socket->send(Message_Handler::encode_msg($arr));
 
-        $out = $socket->recv();
     }
-    public function testResponseVote() {
-
-    }
-
-    public function testResponseOpenCamForProfessor()
+    public function testResponseOpenCamForProfessor ()
     {
-        $arr = array(
-            'action' => 'open_cam_for_prof',
-            'from' => 'proffessor', //|prof|admin
-            'to' => 'student',
-            'device_id' => '1',
-            'student_name' => 'hossam',
-            'execute' => ['']
+        $arr = array
+        (
+            "action" => "failed",
+            "to" => "proffessor",
+            "from" => "student",
+            "student_name" => msg["student_name"],
+            "device_id" => "device_ID",
+            "execute" =>
+                [
+                    "student_id" => "studend_id",
+                    "device_id" => "device_ID",
+                    "studentname" => msg["student_name"],
+                    "cause" => "not in data base"
+                ]
         );
+
+        if ("get_stud_name" == msg["student name"]) {
+            $arr = array
+            (
+                "action" => "done",
+                "to" => "proffessor",
+                "from" => "student",
+                "student_name" => "get_stud_name",
+                "device_id" => "device_ID",
+                "execute" =>
+                    [
+                        "student_id" => "studend_id",
+                        "device_id" => "device_ID",
+                        "student_name" => "getstud_name"
+                    ]
+            );
+
+
+            $arr = array
+            (
+                "action" => "failed",
+                "to" => "proffessor",
+                "from" => "student",
+                "studentname" => "getstudname",
+                "deviceid" => "device_ID",
+                "execute" =>
+                    [
+                        "studentid" => "studendid",
+                        "deviceid" => "device_ID",
+                        "studentname" => "getstud name",
+                        "cause" => "another student"
+                    ]
+            );
+        }
+
 
         $socket = new Socket($this->address, $this->service_port);
         $socket->connect();
@@ -61,21 +163,26 @@ class TestServer extends TestCase
         //receive ok statement
 
     }
-    public function testResponseOpenCamForAdmin() {
+    public function testResponseOpenCamForAdmin     (){
 //                $this->markTestSkipped("waiting to sign new student first");
 
-        $arr = array(
-            'action' => 'open_cam_for_admin',
-            'from' => 'adminstrator', //|prof|admin
-            'to' => 'student',
-            'device_id' => '1',
-            'pic_nums' => 10,
-            'execute' => ['student_name' => 'abdo', 'n.o pictures' => 10]
+        $arr = array
+        (
+                "action"        => "done"                        ,
+                "to"            => "adminstrator"                ,
+                "from"          => "student"                     ,
+                "studen_tname"  => msg["execute"]["student_[name"] ,
+                "device_id"     => "device_ID"                   ,
+                "execute"       =>
+        [       "student_id"    => msg["execute"]["student_ID"]   ,
+                "device_id"     => "device_ID"                   ,
+                "student_name"  => msg["execute"]["student_name"]
+        ]
         );
 
         $socket = new Socket($this->address, $this->service_port);
         $socket->connect();
-//        sleep(5);
+        //sleep(5);
         $socket->send(Message_Handler::encode_msg($arr));
         echo "Closing socket...";
 //        var_dump( Message_Handler::decode_msg($socket->recv()));
@@ -85,7 +192,27 @@ class TestServer extends TestCase
         //receive ok statement
 
     }
-    public function testResponseTrain() {
+    public function testResponseTrain               ()
+    {
+
+
+        $arr = array
+        ([
+            " action"     => " done"           ,
+            " to "        => " adminstrator "  ,
+            " from"       => " none "          ,
+            "student_name" => " none"          ,
+            " device_id"  => " none"           ,
+            " execute"    => " none"
+        ]);
+        $socket = new Socket($this->"address", $this=>"service_port");
+        $socket->connect();
+        $socket->send(Message_Handler::encode_msg($arr));
+
+
+    }
+
+
 
     }
 }
