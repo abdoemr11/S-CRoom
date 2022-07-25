3ee4f86bbdcb483f9d37ccb280e0e0fcbcbc9435
Abo <mahmoudgoda151@outlook.com>
2022-07-25 13:56:02 +0200

    modified some table begin professor session pass now parameter to request

--- S_CRoom_BackEnd/web-application/resources/views/loginRasp.blade.php
+++ S_CRoom_BackEnd/web-application/resources/views/loginRasp.blade.php
@@ -8,20 +8,20 @@
     <h1>LOG IN</h1>
 </header>
 <main>
-    <form id="login_form" class="form_class" action="login.php" method="post">
+    <form id="login_form" class="form_class" action="/stdlive" method="post">
         <div class="form_div">
             <p id="no1">Student ID:</p>
             <input class="field_class , use-keyboard-input"type="number" placeholder="Enter your ID" id="std_id">
             <p id="no1">Prof ID:</p>
             <input class="field_class , use-keyboard-input"type="number" placeholder="Enter Prof ID" id="prof_id">
+
             <button class="submit_class" type="submit" form="login_form" onclick="submit()">Submit</button>
         </div>
-
     </form>
 </main>
 <script src="keyboard.js"></script>
 <script>
-    let ws = new WebSocket("wss://127.0.0.1:8880");
+    let ws = new WebSocket("ws://127.0.0.1:8080");
     function submit() {
         std_id  = document.getElementById("std_id").value;
         prof_id = document.getElementById("prof_id").value;
@@ -30,14 +30,16 @@
             "to": "server",
             "from": "student",
             "execute": {
-                "name": std_id,
-                "professor_id": prof_id
+                "student_id": std_id,
+                "professor_id": prof_id,
+                "device_id": ,
+                "origin" : "javascript"
             }
         }));
-        websocket.onmessage = function (event) {
+        ws.onmessage = function (event) {
             var received_msg = JSON.parse(event.data);
             if (received_msg.execute.status == "OK") {
-                location.href = "//Live Page for student";
+                location.href = "/stdlive";
             }
 
         }
