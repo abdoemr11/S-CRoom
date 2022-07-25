b5f19f06cd33dc9387d5527ef6364b2e549f5782
Abo <mahmoudgoda151@outlook.com>
2022-07-25 15:22:25 +0200

    modified some table begin professor session pass now parameter to request

--- S_CRoom_BackEnd/web-application/resources/views/loginRasp.blade.php
+++ S_CRoom_BackEnd/web-application/resources/views/loginRasp.blade.php
@@ -8,31 +8,32 @@
     <h1>LOG IN</h1>
 </header>
 <main>
-    <form id="login_form" class="form_class" action="/stdlive" method="get">
-        <div class="form_div" style="display: flex">
+
+        <div class="form_class" id="login_form" style="display: flex">
             <div><p id="no1">Student ID:</p>
                 <input class="field_class , use-keyboard-input"type="number" placeholder="Enter your ID" id="std_id"></div>
             <div><p id="no1">Prof ID:</p>
                 <input class="field_class , use-keyboard-input"type="number" placeholder="Enter Prof ID" id="prof_id"></div>
 
-            <button class="submit_class" type="submit" form="login_form" onclick="submit()">Submit</button>
+            <button class="submit_class" form="login_form" onclick="sub()">Submit</button>
         </div>
-    </form>
     <img src="wait.png" style="display: none; height: 200px; width: 200px" id="waiting_img">
 </main>
 <script src="keyboard.js"></script>
 <script>
     let ws = new WebSocket("ws://127.0.0.1:8080");
-    function submit() {
-        let std_id  = document.getElementById("std_id").value;
-        let prof_id = document.getElementById("prof_id").value;
+    console.log("here");
+    let std_id  = document.getElementById("std_id").value;
+    let prof_id = document.getElementById("prof_id").value;
+    function sub() {
+
         ws.send(JSON.stringify({
             "action": "connect",
             "to": "server",
             "from": "student",
             "execute": {
-                "student_id": std_id,
-                "professor_id": prof_id,
+                "student_id": "std_id",
+                "professor_id": "prof_id",
                 "device_id": "123",
                 "origin" : "javascript"
             }
@@ -43,9 +44,6 @@
             let received_msg = JSON.parse(event.data);
             if (received_msg.execute.type ==="verify" && received_msg.execute.status === "OK") {
                 location.href = "/stdlive";
-            }
-            else {
-
             }
 
         }
--- S_CRoom_BackEnd/web-application/resources/views/studentLive.blade.php
+++ S_CRoom_BackEnd/web-application/resources/views/studentLive.blade.php
@@ -4,8 +4,12 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="popper.min.js"></script>
-    <link rel="stylesheet" href="bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="try.css">
     <link rel="stylesheet" type="text/css" href="try.js">
     <link rel="stylesheet" type="text/css" href="style.css">
@@ -80,8 +84,8 @@
         ws.send(JSON.stringify(raise_msg));
     }
     function send_question() {
-        qustion = document.getElementById("quest").value;
-        question_msg={"action" : "std_msg",
+        let qustion = document.getElementById("quest").value;
+        let question_msg={"action" : "std_msg",
             "to": "professor",
             "from" : "student",
             "execute" : {"device_id" : "//student_id","student_id": "student_id","questions" : qustion}};
